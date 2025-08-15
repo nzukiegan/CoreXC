171084a34294df1166acb86d281d3f1f3a23edb8
egan <nzukiegan6@gmail.com>
2025-08-15 17:25:13 +0300

    updates

--- Program.cs
+++ Program.cs
@@ -6,14 +6,36 @@ class Program
 {
     static void Main()
     {
-        string connectionString = "Server=localhost;Database=CoreXCDb;Trusted_Connection=True;";
+        string masterConnection = "Server=localhost;Database=master;Trusted_Connection=True;";
+        string targetDbName = "CoreXCDb";
+
+        using (SqlConnection conn = new SqlConnection(masterConnection))
+        {
+            conn.Open();
+            Console.WriteLine("Connected to master database.");
+
+            string createDbSql = $@"
+                IF NOT EXISTS (SELECT name FROM sys.databases WHERE name = '{targetDbName}')
+                BEGIN
+                    CREATE DATABASE [{targetDbName}];
+                END";
+            
+            using (SqlCommand cmd = new SqlCommand(createDbSql, conn))
+            {
+                cmd.ExecuteNonQuery();
+            }
+
+            Console.WriteLine($"Database '{targetDbName}' is ready.");
+        }
+
+        string targetDbConnection = $"Server=localhost;Database={targetDbName};Trusted_Connection=True;";
 
         string createTablesSql = File.ReadAllText("Schema.sql");
 
-        using (SqlConnection conn = new SqlConnection(connectionString))
+        using (SqlConnection conn = new SqlConnection(targetDbConnection))
         {
             conn.Open();
-            Console.WriteLine("Connected to database!");
+            Console.WriteLine($"Connected to database '{targetDbName}'.");
 
             using (SqlCommand cmd = new SqlCommand(createTablesSql, conn))
             {
